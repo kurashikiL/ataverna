a901ba33d683f3eaeec1b8bde33d6895b21877e8
kurashikiL <leandrogk2@gmail.com>
2021-11-16 12:12:05 -0300

    adicionei post dinamicos e funcao de like

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,30 +1,40 @@
 import "./post.css";
 import {MoreVert} from "@material-ui/icons";
-function Post(){
+import {Users} from "../../dummyData";
+import {useState} from "react";
+
+function Post({post}){
+    const [ like, setLike] = useState(post.like);
+    const [ isLiked, setIsLiked] = useState(false);
+
+    const likeHandler = () =>{
+        setLike(isLiked ? like-1 : like+1)
+        setIsLiked(!isLiked)
+    }
     return(
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
                         <img className = "postProfileImg" src="/assets/person/1.jpg" alt=""/>
-                        <span className = "postUsername">MestreShow</span>
-                        <span className = "postDate">5min</span>
+                        <span className = "postUsername">{Users.filter((u)=>u.id === post.userId)[0].username}</span>
+                        <span className = "postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert/>
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">primeiroPost</span>
-                    <img className="postImg" src="/assets/person/1.jpg" alt=""/>
+                    <span className="postText">{post?.desc}</span>
+                    <img className="postImg" src={post?.photo} alt=""/>
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className="likeIcon" src="assets/d20.png" alt="" />
-                        <span className="postDisikeCounter">5</span>      
+                        <img className="likeIcon" src="assets/d20.png" onClick={likeHandler} alt="" />
+                        <span className="postLikeCounter">{like}</span>      
                     </div>
                         <div className="postBottomRight">
-                        <span className="postCommentText">5 comentários</span>
+                        <span className="postCommentText">{post.comment}</span>
                     </div>
                 </div>
             </div>
---
+++ src/dummyData.js
@@ -0,0 +1,47 @@
+export const Users = [
+    {
+        id: 1,
+        profilePic: "/assets/person/1.jpg",
+        username: "MrPickles"
+    },
+    {
+        id: 2,
+        profilePic: "/assets/person/1.jpg",
+        username: "Erin"
+    },
+    {
+        id: 3,
+        profilePic: "/assets/person/1.jpg",
+        username: "Jojo"
+    }
+]
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "Oi site",
+        photo: "/assets/person/1.jpg",
+        date: "5min",
+        userId: 1,
+        like: 32,
+        comment:2
+    },
+    {
+        id: 2,
+        desc: "Oi site",
+        photo: "/assets/person/1.jpg",
+        date: "5min",
+        userId: 1,
+        like: 15,
+        comment:4
+    },
+    {
+        id: 3,
+        desc: "Oi site",
+        photo: "/assets/person/1.jpg",
+        date: "5min",
+        userId: 2,
+        like: 10,
+        comment:6
+    },
+]
