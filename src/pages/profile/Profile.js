ab287ac928b9577ce3a17bfe465c7c941c36ca18
kurashikiL <leandrogk2@gmail.com>
2021-11-16 15:46:59 -0300

    adicionei tela profile

---
+++ src/pages/profile/Profile.js
@@ -0,0 +1,35 @@
+import "./profile.css"
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+
+function Profile(){
+    return(
+        <>
+            <Topbar/>
+            <div className="profileContainer">
+                <Sidebar/>
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img className="profileCoverImg" src="/assets/person/1.jpg" alt=""/>
+                            <img className="profilePicImg" src="/assets/person/1.jpg" alt=""/>     
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">MrPickles</h4>
+                            <span className="profileInfoDesc">Mestre de DnD</span>
+                        </div>
+                    </div>
+                
+                    <div className="profileRightBottom">
+                        <Feed/>
+                        <Rightbar profile/>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+}
+export default Profile;
