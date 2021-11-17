77e7d879bd8f3960dd401089c88f372576a53b70
Draft-dev <LuizE.puc@outlook.com>
2021-11-17 02:12:26 -0300

    cadastro com verificação, precisa de atualização...

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,7 +1,19 @@
 import "./register.css"
+import {userSchema} from "../../validation/userValidation.js";
 import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Input } from "@material-ui/core";
 
 function Register(){
+  
+
+    const {register,handleSubmit,formState: { errors }} = useForm({
+        resolver: yupResolver(userSchema),
+    });
+
+    const submitForm = data => console.log(data);
+
     return(
         <div className="register">
             <div className="registerLeft">
@@ -22,21 +34,31 @@ function Register(){
                         <div className ="registerSelected">Cadastrar</div>
                          
                     </div>
-                    <div className="registerInputs">
+                    <form onSubmit={handleSubmit(submitForm)}>
+                        <div className="registerInputs">
+                            
+                                <input name="name" placeholder="Nome ou apelido"  {...register('name')}></input>
+                                {errors.name && "Todos os campos são obrigatórios!" 
+                                || errors.login && "Todos os campos são obrigatórios!"
+                                || errors.email && "Todos os campos são obrigatórios!"
+                                || errors.password && "Todos os campos são obrigatórios!"
+                                || errors.confirmPassword && errors.confirmPassword.type==="required" && "Todos os campos são obrigatórios!"}
+                                <br></br>                               
+                                <input name="login" placeholder="Login" {...register('login')}></input>
+                                {errors.login && "Esse campo é obrigatório"}
+                                <br></br>
+                                <input name="email" placeholder="E-mail" {...register('email')}></input>
+                                {errors.email && "Insira um e-mail válido"}
+                                <br></br>
+                                <input name="password" type="password"  placeholder="Senha" {...register('password')}></input>
+                                {errors.password && "Esse campo é obrigatório"}
+                                <br></br>
+                                <input name="confirmPassword" type="password"  placeholder="Confirmar Senha" {...register('confirmPassword')}></input>
+                                {errors.confirmPassword && "Senhas não são iguais"}
 
-                        <input placeholder="Nome Completo"></input>
-                        <br></br>
-                        <input placeholder="Login"></input>
-                        <br></br>
-                        <input placeholder="E-mail"></input>
-                        <br></br>
-                        <input type="password"  placeholder="Senha"></input>
-                        <br></br>
-                        <input type="password"  placeholder="Confirmar Senha"></input>
-
-                    </div>
-                    <button className="enterButton"> Cadastrar </button>
-                    
+                        </div>
+                        <input type="submit" name="submit" className="enterButton" value="Cadastrar"></input>
+                    </form>
                 </div>
             </div>
 
